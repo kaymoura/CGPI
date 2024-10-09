9438a9ea9968fb15ad38c5afba1061ba826d650d
Kaylane <91162976+kaymoura@users.noreply.github.com>
2024-10-09 20:38:15 -0300

    v0.1

---
+++ Atividade Filtros/ex1.py
@@ -0,0 +1,63 @@
+#Atividade
+import cv2
+import numpy as np
+## from google.colab.patches import cv2_imshow # Import the cv2_imshow function
+import matplotlib.pyplot as plt # Importa a biblioteca matplotlib e crie um alias para ela como plt
+
+#Carrega a imagem da placa e coloca em tons de cinza
+imagem = cv2.imread('C:/Users/user/Documents/GitHub/CGPI/Atividade Filtros/placa.png', cv2.IMREAD_GRAYSCALE)
+
+#Aplicando Filtro Média (Suaviza)
+filtro_media_3x3 = cv2.blur(imagem, (3, 3))
+filtro_media_15x15 = cv2.blur(imagem, (6, 6))
+
+#Aplicando Filtro Mediana (Suaviza)
+filtro_gaussiano_mediana_7 = cv2.GaussianBlur(imagem, (5,5),0)
+filtro_gaussiano_mediana_15 = cv2.GaussianBlur(imagem, (11,11),0)
+#filtro_mediana_7 = cv2.medianBlur(imagem, 7)
+#filtro_mediana_15 = cv2.medianBlur(imagem, 15)
+
+#Aplicando Filtro Sobel (Destaca as bordas)
+sobel_x = cv2.Sobel(imagem, cv2.CV_64F, 1, 0, ksize=3)
+sobel_y = cv2.Sobel(imagem, cv2.CV_64F, 0, 1, ksize=3)
+sobel_combinado = cv2.addWeighted(cv2.convertScaleAbs(sobel_x), 0.5, cv2.convertScaleAbs(sobel_y), 0.5, 0)
+
+
+# Exibir os Resultados:
+plt.figure(figsize=(15, 10))
+
+# Imagem Original
+plt.subplot(3, 3, 1)
+plt.title('Imagem Original')
+plt.imshow(imagem, cmap='gray')
+plt.axis('off')
+
+# Filtros de Média
+plt.subplot(3, 3, 2)
+plt.title('Filtro Média 7x7')
+plt.imshow(filtro_media_3x3, cmap='gray')
+plt.axis('off')
+
+plt.subplot(3, 3, 3)
+plt.title('Filtro Média 15x15')
+plt.imshow(filtro_media_15x15, cmap='gray')
+plt.axis('off')
+
+# Filtros de Mediana
+plt.subplot(3, 3, 5)
+plt.title('Filtro Mediana 7')
+plt.imshow(filtro_gaussiano_mediana_7, cmap='gray')
+plt.axis('off')
+
+plt.subplot(3, 3, 6)
+plt.title('Filtro Mediana 15')
+plt.imshow(filtro_gaussiano_mediana_15, cmap='gray')
+plt.axis('off')
+
+# Filtros Sobel
+plt.subplot(3, 3, 4)
+plt.title('Filtro Sobel X + Y')
+plt.imshow(sobel_combinado, cmap='gray')
+plt.axis('off')
+
+plt.show()
---
+++ Atividade Filtros/explicação.md
@@ -0,0 +1,31 @@
+### Atividade Filtros
+
+1 Carregar uma Imagem OK (Linha 08 do código)
+
+
+2. Aplicar os Filtros:
+	1. Observar as mudanças na imagem original após a aplicação dos filtros de média, mediana e Sobel.
+		Média:
+		Mediana:
+		Sobel:
+
+
+3. Analisar os Resultados:
+	1. Responder às seguintes perguntas:
+		a) Como a imagem original mudou após a aplicação de cada filtro?
+		R_	
+		b) Qual filtro foi mais eficaz para suavizar a imagem?
+		R_ A imagem original...
+		c) E qual foi mais eficaz para destacar as bordas?
+		R_
+		d) Quais situações podem exigir o uso de cada tipo de filtro em um projeto real?
+4. Experimentar:
+
+
+1. Experimentar diferentes tamanhos de kernel nos filtros de média e mediana (por exemplo, (3, 3), (5, 5), etc.) e observar como isso afeta a suavização da imagem.
+
+ Experimento 1 - filtros de média (3,3)(6,6) e mediana (5,5)(10,10) - Resultado na figura 2
+Com essa tentativa notei que o kernel da mediana não executa pois ele necessariamente necessita ser um número impar para ter o centro da matriz. Então alterei o (10,10) para (11,11). 
+
+ Experimento 2 - filtros de média (4,4)(2,2) e mediana (9,9)(17,17) - Resultado na figura 3
+
